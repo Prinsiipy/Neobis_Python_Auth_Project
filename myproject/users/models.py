fea3d0e2e65f9627ae60249daa96e9ee9fbd2055
isa <chomoevisa@gmail.com>
2023-04-07 18:41:55 +0600

    Merge remote-tracking branch 'origin/master'

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+    )
+
+    email_verify = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
